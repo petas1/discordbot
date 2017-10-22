fbb86709b3f19e96d5e93ac0018dff8b34082027
petas1 <32804106+petas1@users.noreply.github.com>
2017-10-22 19:30:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeTaS_Bot_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBot bot = new MyBot();    
+        }
+    }
+}
